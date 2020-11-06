00a28ee8717cfa9b1051dfdcead0609c4f60252f
Alina Iacob <alinai@j2inn.com>
2020-11-06 14:14:29 +0200

    pearsonTest finished

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # pearsonTest
+INTRODUCTION
+------------
+This test if for verifying the Search bar and its functionality and if the correct link is opened after an article is accessed.
+
+Setup For Javascript Automation with Selenium
+Firstly, we w’ll do the the setup for Javascript automation. Below are the steps which we will follow
+
+REQUIREMENTS
+------------
+This project requires the following modules:
+
+- Node.js
+- Selenium Webdriver
+- Mocha
+
+INSTALLATION
+------------
+- Install Node.js
+- Verify the installation of Node.js
+- Create package.json file
+- Install Selenium Webdriver
+- Install Mocha
+
+Install Node.js
+Before installing Node.js its important to understand that what is Node.js and why it is used. It is a tool because it provide the environment to run the JS code.
+
+https://nodejs.org/en/download/
+
+Verify the installation
+If you done with installation of Node.js , After that follow the below simple steps to verify the installation:
+
+Open CMD on windows.
+Type command: node -v
+
+
+Create package.json file
+Just create an Empty folder with your project name and open terminal (Command prompt) window in that folder.
+
+Type in VS Code terminal: 
+npm init
+Hit enter
+Next, npm ask some questions related to configuring your package.json file. Just provide the information npm asking for otherwise, you can hit enter to skip. Later, you can configure it according to your project. For now,  just provide name, description, and author. After entering that just type Yes to save the file and hit enter.
+
+Add npm packages to package.json
+What dependencies we need to set up for this automation project setup.
+
+1. selenium-webdriver
+npm install selenium-webdriver
+2. chromedriver
+npm install chromedriver
+3. mocha
+npm install mocha
+4. @types packages
+- @types/node
+- @types/mocha
+- @types/selenium-webdriver
+npm install @types/node
+npm install @types/mocha
+npm install @types/selenium-webdriver
+
+How to run test
+----------------
+After cloning the repo locally, go to the project’s folder using a terminal.
+Run this command: 'npm run startTest'
---
+++ test/searchFunctionality.js
@@ -0,0 +1,48 @@
+// Import requirement packages
+require('chromedriver');
+const {Builder, Key, By, until, Capabilities} = require('selenium-webdriver');
+const caps = new Capabilities();
+
+describe('Access pearson.com', function () {
+    let driver;
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();    
+    });
+
+    it('Search on pearson.com', async function() {
+    //Maximize current window
+        driver.manage().window().maximize();
+    // Load the page
+        await driver.get('https://www.pearson.com/');
+    //Accept cookies
+        await driver.findElement(By.id('cookie-notification-policy-accept-continue')).click();
+    // Find the search box by id
+        await driver.wait(until.elementLocated(By.id('search-box-input')), 10000);
+        await driver.findElement(By.id('search-box-input')).click();
+    // Enter keywords and click 'Enter'
+        await driver.findElement(By.id('search-box-input')).sendKeys('software testing', Key.ENTER);
+    //Wait the total of search results and count them
+        await driver.wait(until.elementLocated(By.xpath("//div[@class='st-results-container']//article")), 10000); 
+        driver.findElements(By.xpath("//article")).then(elements => console.log(elements.length));
+    // Scroll to the Next button and click it
+        let element = driver.findElement(By.xpath("//ul[@class='pagination']//li[7]"));
+        driver.executeScript("arguments[0].scrollIntoView(true);",element);
+        await driver.findElement(By.xpath("//ul[@class='pagination']//li[7]")).click();
+    //Wait the total of search results from the second page and count them
+        await driver.wait(until.elementLocated(By.xpath("//div[@class='st-results-container']//article")), 10000); 
+        driver.findElements(By.xpath("//article")).then(elements => console.log(elements.length));    
+    // Scroll to the third article on page and open it
+        await driver.wait(until.elementLocated(By.xpath("//div[@class='st-results-container']")), 10000);
+        let article = driver.findElement(By.xpath("//article[3]//p[@class='productItem__description']"));
+        driver.executeScript("arguments[0].scrollIntoView(true);",article);
+        await driver.wait(until.elementLocated(By.xpath("//article[@class='productItem test'][3]//a[contains(@href, '://')]")), 10000);
+        await driver.findElement(By.xpath("//article[@class='productItem test'][3]//a[contains(@href, '://')]")).click();
+        driver.wait(function() {
+            return driver.getTitle().then(function(title) {
+              return title === 'December trading update';
+            });
+            }, 1000);
+    });
+    // Close the browser after running tests
+    after(() => driver && driver.quit());
+})
